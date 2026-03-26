7c549102bd0133de2382ea99aa345218bee100dc
abhishekpatel124 <abhishekietian19@gmail.com>
2026-03-26 10:32:46 +0530

    first step completed

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// app.get("/", (req, res) => {mkdir public\javascripts, public\stylesheets
+//     res.send("Hello World!");
+// });
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
 });
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render("main");
+});
+
+app.get("/stylesheets/style.css", (req, res) => {
+    res.send("Hello World!");
+});
+
--- readme.md
+++ readme.md
@@ -1,17 +1,22 @@
 ```
  ## 1. Initialize a project with npm
-## npm init -y
+# npm init -y
 
 ```
 ```
-2. Express install
-   npm i express
-
-```
+## 2. Express install
+# npm i express
 
 ```
 ## to tun the file index.js 
 # npx nodemon index.js
+```
 
-```# first_backend_project
-# first_backend_project
+
+
+
+```
+## to install ejs 
+
+# npm i ejs
+```
